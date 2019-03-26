7b404e5d51f417f9b69028c3b6f10b601aad9d09
yj <218wkwk@gmail.com>
2019-03-26 16:33:04 +0900

    MemberVO 생성

---
+++ gittest2/src/gittest2/MemberVO.java
@@ -0,0 +1,25 @@
+package gittest2;
+
+public class MemberVO {
+	private String name;
+	private String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public MemberVO(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	
+
+}
