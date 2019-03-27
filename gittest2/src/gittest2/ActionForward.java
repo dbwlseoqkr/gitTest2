5732d3351d5122ab053c75e9decaea6f46dedc94
yj <218wkwk@gmail.com>
2019-03-27 10:05:18 +0900

    actionforward추가

--- gittest2/src/gittest2/ActionForward.java
+++ gittest2/src/gittest2/ActionForward.java
@@ -3,5 +3,17 @@ package gittest2;
 public class ActionForward {
 	private String path;
 	private boolean isRediret;
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public boolean isRediret() {
+		return isRediret;
+	}
+	public void setRediret(boolean isRediret) {
+		this.isRediret = isRediret;
+	}
 
 }
